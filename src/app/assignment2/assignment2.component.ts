0726a88bdb730425cdcd8d30602e2b13e2c8cc2f
hawrot <mateusz@sent.com>
2018-09-27 15:10:44 +0100

    assignment2

---
+++ src/app/assignment2/assignment2.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {ifTrue} from "codelyzer/util/function";
+
+@Component({
+  selector: 'app-assignment2',
+  templateUrl: './assignment2.component.html',
+  styleUrls: ['./assignment2.component.css']
+})
+export class Assignment2Component implements OnInit {
+
+  username = '';
+  allowUsername = false;
+  userCreationStatus = 'user not created';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+
+  onAddUsername(){
+    this.userCreationStatus = 'user was created! Name is: ' + this.username ;
+  }
+
+}
